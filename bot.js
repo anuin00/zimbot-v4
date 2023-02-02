68ddd0c1c08f2258613abadb1b4e7082d457b8d6
anuin00 <123932733+anuin00@users.noreply.github.com>
2023-02-02 21:28:23 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,17 +33,17 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
+global.owner = ['62895335330202',''] //owner number +27634090203
+global.wame = ['62895335330202'] 
+global.pemilik = ['62895335330202'] //owner number +27634090203
+global.premium = ['62895335330202'] //premium number +27634090203
 global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.footer = 'anu1'
+global.botnma = 'anu bot' //bot name
+global.botname = 'anu2'
+global.ownernma = 'anu3' //owner name
+global.packname = 'anu4' //sticker package name
+global.author = 'anu5' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
